94af63792ea9db9cdde36f6102dbbf4110d253d3
Trent Stanley <tstanley@lcs.on.ca>
2020-04-08 13:44:25 -0400

    Initial Commit

---
+++ Project1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by Stanley, Trent on 2020-04-08.
+//  Copyright © 2020 Stanley, Trent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
